95d39728dca304f334ef10312b0259e76dfc2000
Jack36VRN <jack3@JACK36-LAPTOP>
2019-09-27 20:03:42 +0300

    Update footer.php

--- login/footer.php
+++ login/footer.php
@@ -1,4 +1,4 @@
-<footer class="mt-5"><small>Версия: 0.0.1</small></footer>
+<footer class="mt-5"><small>Версия: 0.0.1.1</small></footer>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
